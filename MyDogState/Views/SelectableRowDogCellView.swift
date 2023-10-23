0ea04365e1f132f1d32fe08eb5dac8561c3d9bcc
francescarossi <francesca.rossi15@studenti.unipr.it>
2023-10-23 16:48:54 +0200

    Fix change state selectable row

---
+++ MyDogState/Views/SelectableRowDogCellView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectableRowDogCellView.swift
+//  MyDogState
+//
+//  Created by Francesca Rossi on 23/10/23.
+//
+
+import SwiftUI
+
+struct SelectableRowDogCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectableRowDogCellView()
+}
