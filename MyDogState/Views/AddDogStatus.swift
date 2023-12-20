4fb96b992e9fd45eb9bac510733d3d320ae14c9e
francescarossi <francesca.rossi15@studenti.unipr.it>
2023-12-20 15:47:01 +0100

    Create Add dog status view

---
+++ MyDogState/Views/AddDogStatus.swift
@@ -0,0 +1,41 @@
+//
+//  AddDogStatus.swift
+//  MyDogState
+//
+//  Created by Francesca Rossi on 20/12/23.
+//
+
+import SwiftUI
+
+struct AddDogStatus: View {
+    var image: UIImage
+    @State var note: String
+    var body: some View {
+        VStack
+        {
+            Text("Page title")
+            
+            RoundedRectagleImage(image: image)
+            Form  {
+                Section(header: Text("Note"))
+                {
+                    
+                    TextEditor(text: $note)
+                }
+            }
+            Button(action: {//TODO:
+            })
+            {
+                HStack
+                {
+                    Image(systemName: "square.and.arrow.down")
+                    Text("save in the history")
+                }
+            }.buttonStyle(AnimatedCapsuleBlueButton())
+        }
+    }
+}
+
+#Preview {
+    AddDogStatus(image: UIImage(), note: "")
+}
