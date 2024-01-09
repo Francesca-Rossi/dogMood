b291ea50df57764780a4d0e7650ddf72528a338c
francescarossi <francesca.rossi15@studenti.unipr.it>
2024-01-09 07:46:45 +0100

    create dogDetailCard

---
+++ MyDogState/Views/DogDetailCard.swift
@@ -0,0 +1,18 @@
+//
+//  DogDetailCard.swift
+//  MyDogState
+//
+//  Created by Francesca Rossi on 08/01/24.
+//
+
+import SwiftUI
+
+struct DogDetailCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DogDetailCard()
+}
