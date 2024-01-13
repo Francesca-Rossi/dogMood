ab35f1b3ed959fc942ac6f737c49771790b16d8c
francescarossi <francesca.rossi15@studenti.unipr.it>
2024-01-13 15:49:02 +0100

    Create checkDetailView

---
+++ MyDogState/Views/CheckDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MoodDetailView.swift
+//  MyDogState
+//
+//  Created by Francesca Rossi on 13/01/24.
+//
+
+import SwiftUI
+
+struct CheckDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckDetailView()
+}
